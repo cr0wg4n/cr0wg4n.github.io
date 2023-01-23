2dfac5d246dadaf92e002e3d844965c2c4f17ad0
Mauricio M <mcm.crw@gmail.com>
2023-01-23 12:06:10 -0400

    fix: removing helmet

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import ReactGa from 'react-ga4';
 import { useEffect } from 'react';
 import Clothes from './components/Clothes/Clothes';
 import Control from './components/Boxes/Control';
-import Helmet from 'react-helmet';
 
 function App() {
   const profile = useAppSelector(selectProfile);
@@ -19,23 +18,8 @@ function App() {
     ReactGa.send({ hitType: "pageview", page: "/" });
   },[])
 
-  const description = `Hi, I'm Mauricio. Technology Building and designing is my main passion. Currently, I'm learning about systems programming and Rust for fun. I love and follow the Open Source and Free Sofware initiatives. I occasionally instruct programming with Python and I share my day-to-day tech experience through articles and talks.`
-
   return (
     <div className='bg-blue-gray-900 p-4 md:p-10 font-ubuntu text-blue-gray-50 relative h-full'>
-      <Helmet>
-          <html lang="en" />
-          <title>Mauricio M. - Website</title>
-          <meta name="description" content={description} />
-          <meta name="theme-color" content="#ffffff" />
-
-          <meta property='og:title' content='Mauricio M. - Website' />
-          <meta property='og:url' content='https://cr0wg4n.github.io/' />
-          <meta property='og:description' content={description} />
-          <meta property="twitter:site" content="@cr0wg4n" />
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property='twitter:description' content={description} />
-      </Helmet>
 
       <Control />
 
