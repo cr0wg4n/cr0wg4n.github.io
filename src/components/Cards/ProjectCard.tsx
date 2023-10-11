92c71d54189ce591bb3d3a36ab89a3be03607d34
Mauricio M <mcm.crw@gmail.com>
2023-10-10 22:11:08 -0400

    fix, alt data

--- src/components/Cards/ProjectCard.tsx
+++ src/components/Cards/ProjectCard.tsx
@@ -14,7 +14,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
 }: ProjectCardProps) => {
   return <div className='mt-4 relative hover:cursor-pointer hover:border-b-2' title={description}>
     <a href={url} target={'_blank'} rel={"noreferrer"}>
-      <img src={cover} width={'100%'} alt='ehc group main page' className='h-40 rounded-t-lg object-cover' />
+      <img src={cover} width={'100%'} alt={name} className='h-40 rounded-t-lg object-cover' />
       <div className='absolute before:content-[""] bottom-0 bg-black w-full opacity-80 px-2 py-1 font-quicksand md:text-sm text-xs'>
         {name}
       </div>
